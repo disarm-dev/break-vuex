b58aa601716f2c2893339c867475f215360685f1
Jonathan Smith <jonathan@peoplesized.com>
2017-11-15 21:51:37 +0200

    init and built

---
+++ src/App.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <input type="number" v-model="push_count" placeholder="Add new records">
+    <button @click="add_page">Add page</button>
+    <button @click="add_store">Add store</button>
+    <div>
+      <label>Freeze</label>
+      <input type="checkbox" v-model="freeze">
+    </div>
+
+    <div class="flex-container">
+      <div class="flex-item">
+        <h2>Vue Data</h2>
+        <h3>{{page_records.length}} page rows</h3>
+        <button @click="clear_page">Clear</button>
+
+        <ul>
+          <li v-for="{id} in reversed_page_records">{{id}}</li>
+        </ul>
+
+      </div>
+      <div class="flex-item">
+        <h2>Vuex Store</h2>
+        <button @click="clear_store">Clear</button>
+
+        <h3>{{store_records.length}} store rows</h3>
+        <ul>
+          <li v-for="{id} in reversed_store_records">{{id}}</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import {mapState} from 'vuex'
+  import {times} from 'lodash'
+
+  import {fake_record} from './faker'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        page_records_count: 0,
+        page_records: [],
+
+        push_count: 2000,
+        freeze: true
+      }
+    },
+    computed: {
+      ...mapState({
+        store_records: state => state.records
+      }),
+      reversed_page_records() {
+        return this.page_records.slice(-20)
+      },
+      reversed_store_records() {
+        return this.store_records.slice(-20)
+      }
+    },
+    methods: {
+      add_page() {
+        times(this.push_count, () => {
+          this.page_count = (this.page_records.length + 1)
+          let response = fake_record(this.page_count)
+          if (this.freeze) response = Object.freeze(response)
+          this.page_records.push(response)
+        })
+      },
+      clear_page() {
+        this.page_records = []
+      },
+      add_store() {
+        this.$store.commit('push_times', this.push_count, this.freeze)
+      },
+      clear_store() {
+        this.$store.commit('clear')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .flex-container {
+    display: flex;
+    flex-direction: row;
+  }
+
+  .flex-item {
+    flex: auto;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,35 @@
+import {times} from 'lodash'
+
+import Vue from 'vue'
+import App from './App.vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+import {fake_record} from './faker'
+
+const store = new Vuex.Store({
+  state: {
+    count: 0,
+    records: []
+  },
+  mutations: {
+    clear(state) {
+      state.records = []
+    },
+    push_times(state, count, freeze) {
+      times(count, () => {
+        state.count = (state.records.length + 1)
+        let response = fake_record(state.count)
+        if (freeze) response = Object.freeze(response)
+        state.records.push(response)
+      })
+    }
+  }
+})
+
+
+new Vue({
+  el: '#app',
+  store,
+  render: h => h(App)
+})
